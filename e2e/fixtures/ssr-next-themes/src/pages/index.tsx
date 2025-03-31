1976df20cf5eabf2f273e625a87ac453740eed3a
Alex Yang <himself65@outlook.com>
2025-03-30 17:12:06 -0700

    test: update

--- e2e/fixtures/ssr-next-themes/src/pages/index.tsx
+++ e2e/fixtures/ssr-next-themes/src/pages/index.tsx
@@ -1,10 +1,9 @@
 'use client';
 import { useTheme } from 'next-themes';
 export default function Page() {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme: theme, setTheme } = useTheme();
   return (
     <div>
-      {theme}
       <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
         Toggle theme
       </button>
