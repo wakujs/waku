2a899a6f08ccf6641cb9e6ff65a452aba6baa205
Tyler <26290074+tylersayshi@users.noreply.github.com>
2025-04-01 20:52:28 -0700

    fix(template): use link stylesheet for loading google font for now

--- examples/01_template/src/pages/_layout.tsx
+++ examples/01_template/src/pages/_layout.tsx
@@ -14,6 +14,10 @@ export default async function RootLayout({ children }: RootLayoutProps) {
     <div className="font-['Nunito']">
       <meta name="description" content={data.description} />
       <link rel="icon" type="image/png" href={data.icon} />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+      />
       <Header />
       <main className="m-6 flex items-center *:min-h-64 *:min-w-64 lg:m-0 lg:min-h-svh lg:justify-center">
         {children}
