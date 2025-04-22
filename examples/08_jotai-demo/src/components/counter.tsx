3e1fa1a501c7abed5f23d80440c92f2971162025
daishi <daishi@axlight.com>
2025-04-22 11:19:57 +0900

    add useTransition

--- examples/08_jotai-demo/src/components/counter.tsx
+++ examples/08_jotai-demo/src/components/counter.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useTransition } from 'react';
 import { unstable_allowServer as allowServer } from 'waku/client';
 import { atom, useAtom } from 'jotai';
 
@@ -9,9 +9,14 @@ export const countAtom = allowServer(atom(1));
 export const Counter = () => {
   const [count, setCount] = useState(0);
   const [jotaiCount, setJotaiCount] = useAtom(countAtom);
+  const [isPending, startTransition] = useTransition();
 
   const handleIncrement = () => setCount((c) => c + 1);
-  const handleJotaiIncrement = () => setJotaiCount((c) => c + 1);
+  const handleJotaiIncrement = () => {
+    startTransition(() => {
+      setJotaiCount((c) => c + 1);
+    });
+  };
 
   return (
     <section className="border-blue-400 -mx-4 mt-4 rounded-sm border border-dashed p-4">
@@ -27,7 +32,7 @@ export const Counter = () => {
         onClick={handleJotaiIncrement}
         className="rounded-xs bg-black px-2 py-0.5 text-sm text-white"
       >
-        Jotai Increment
+        Jotai Increment{isPending ? '...' : ''}
       </button>
     </section>
   );
