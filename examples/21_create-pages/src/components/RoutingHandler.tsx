004618ddabc144e733fbc1a0a79ad892a6aed6be
Rob Marscher <rob@robmarscher.com>
2025-04-25 22:00:01 -0400

    Simplify the route change event handler example

--- examples/21_create-pages/src/components/RoutingHandler.tsx
+++ examples/21_create-pages/src/components/RoutingHandler.tsx
@@ -1,18 +1,15 @@
 'use client';
 
-import { useEffect } from 'react';
 import { useRouter } from 'waku/router/client';
 
 export const RoutingHandler = () => {
   const { unstable_onRouteChangeComplete, unstable_onRouteChangeStart } =
     useRouter();
-  useEffect(() => {
-    unstable_onRouteChangeStart((...args) => {
-      console.log('onRouteChangeStart', args);
-    });
-    unstable_onRouteChangeComplete((...args) => {
-      console.log('onRouteChangeComplete', args);
-    });
-  }, [unstable_onRouteChangeComplete, unstable_onRouteChangeStart]);
+  unstable_onRouteChangeStart((...args) => {
+    console.log('onRouteChangeStart', args);
+  });
+  unstable_onRouteChangeComplete((...args) => {
+    console.log('onRouteChangeComplete', args);
+  });
   return null;
 };
