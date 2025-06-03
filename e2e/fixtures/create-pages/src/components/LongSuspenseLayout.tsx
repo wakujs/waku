5b11981559fb7a6d8a59a1d405f3abbb5d68f7c4
Rob Marscher <rob@robmarscher.com>
2025-06-03 10:15:59 -0400

    Add client wrapper component for router events

--- e2e/fixtures/create-pages/src/components/LongSuspenseLayout.tsx
+++ e2e/fixtures/create-pages/src/components/LongSuspenseLayout.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import type { ReactNode } from 'react';
 import { Link } from 'waku';
+import { RouterEvents } from './RouterEvents.js';
 
 export const SlowComponent = async ({ children }: { children?: ReactNode }) => {
   await new Promise((resolve) => setTimeout(resolve, 500));
@@ -17,50 +18,54 @@ export const StaticLongSuspenseLayout = ({
   children: ReactNode;
 }) => {
   return (
-    <div>
-      <h2>Static Long Suspense Layout</h2>
+    <RouterEvents>
       <div>
-        <Link
-          to="/static-long-suspense/5"
-          unstable_pending={
-            <div data-testid="long-suspense-pending">Pending...</div>
-          }
-        >
-          Click Me
-        </Link>
+        <h2>Static Long Suspense Layout</h2>
+        <div>
+          <Link
+            to="/static-long-suspense/5"
+            unstable_pending={
+              <div data-testid="long-suspense-pending">Pending...</div>
+            }
+          >
+            Click Me
+          </Link>
+        </div>
+        <div>
+          <Link to="/static-long-suspense/6">Click Me Too</Link>
+        </div>
+        <Suspense fallback={<div data-testid="long-suspense">Loading...</div>}>
+          <SlowComponent />
+          {children}
+        </Suspense>
       </div>
-      <div>
-        <Link to="/static-long-suspense/6">Click Me Too</Link>
-      </div>
-      <Suspense fallback={<div data-testid="long-suspense">Loading...</div>}>
-        <SlowComponent />
-        {children}
-      </Suspense>
-    </div>
+    </RouterEvents>
   );
 };
 
 export const LongSuspenseLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-      <h2>Long Suspense Layout</h2>
+    <RouterEvents>
       <div>
-        <Link
-          to="/long-suspense/2"
-          unstable_pending={
-            <div data-testid="long-suspense-pending">Pending...</div>
-          }
-        >
-          Click Me
-        </Link>
+        <h2>Long Suspense Layout</h2>
+        <div>
+          <Link
+            to="/long-suspense/2"
+            unstable_pending={
+              <div data-testid="long-suspense-pending">Pending...</div>
+            }
+          >
+            Click Me
+          </Link>
+        </div>
+        <div>
+          <Link to="/long-suspense/3">Click Me Too</Link>
+        </div>
+        <Suspense fallback={<div data-testid="long-suspense">Loading...</div>}>
+          <SlowComponent />
+          {children}
+        </Suspense>
       </div>
-      <div>
-        <Link to="/long-suspense/3">Click Me Too</Link>
-      </div>
-      <Suspense fallback={<div data-testid="long-suspense">Loading...</div>}>
-        <SlowComponent />
-        {children}
-      </Suspense>
-    </div>
+    </RouterEvents>
   );
 };
