fbe9cce4ef7a41968dcf9a9caa706871d9cb41a3
Alex Yang <himself65@outlook.com>
2025-06-16 18:16:09 +0800

    Revert "fix: hydration error with useId (#1109)"
    This reverts commit 67775de6dfb5fe606e1a7086f54f7b9150cbb86e.
    
    # Conflicts:
    #	e2e/ssr-basic.spec.ts
    #	packages/waku/src/lib/renderers/html.ts
    #	packages/waku/src/minimal/client.ts

--- e2e/fixtures/ssr-basic/src/components/Counter.tsx
+++ e2e/fixtures/ssr-basic/src/components/Counter.tsx
@@ -1,21 +1,16 @@
 'use client';
 
-import { useId, useState } from 'react';
+import { useState } from 'react';
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
-  const id = useId();
   return (
     <div
       data-testid="counter"
       style={{ border: '3px blue dashed', margin: '1em', padding: '1em' }}
     >
       <p data-testid="count">{count}</p>
-      <button
-        id={id}
-        data-testid="increment"
-        onClick={() => setCount((c) => c + 1)}
-      >
+      <button data-testid="increment" onClick={() => setCount((c) => c + 1)}>
         Increment
       </button>
     </div>
