f045b79f9429e1d5e8e05cb9ac23b9bd1292639f
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-06-23 10:40:49 -0400

    Fix new e2e test

--- e2e/wildcard-api-routes.spec.ts
+++ e2e/wildcard-api-routes.spec.ts
@@ -4,28 +4,27 @@ import { test, prepareStandaloneSetup } from './utils.js';
 
 const startApp = prepareStandaloneSetup('wildcard-api-routes');
 
-for (const mode of ['DEV', 'PRD'] as const) {
-  test.describe(`wildcard api routes: ${mode}`, async () => {
-    let port: number;
-    let stopApp: () => Promise<void>;
-    test.beforeAll(async () => {
-      ({ port, stopApp } = await startApp(mode));
-    });
-    test.afterAll(async () => {
-      await stopApp();
-    });
-
-    test(`works`, async ({ page }) => {
-      // index route matches wildcard:
-      await page.goto(`http://localhost:${port}/foo`);
-      await expect(page.getByRole('heading', { name: '/foo' })).toBeVisible();
-
-      // api route request
-      const response = await page.request.get(
-        `http://localhost:${port}/api/greet`,
-      );
-      const text = await response.text();
-      expect(text).toBe('Hello, world!');
-    });
+test.describe(`wildcard api routes`, async () => {
+  let port: number;
+  let stopApp: () => Promise<void>;
+  test.beforeAll(async ({mode}) => {
+    ({ port, stopApp } = await startApp(mode));
   });
-}
+  test.afterAll(async () => {
+    await stopApp();
+  });
+
+  test(`works`, async ({ page }) => {
+    // index route matches wildcard:
+    await page.goto(`http://localhost:${port}/foo`);
+    await expect(page.getByRole('heading', { name: '/foo' })).toBeVisible();
+
+    // api route request
+    const response = await page.request.get(
+      `http://localhost:${port}/api/greet`,
+    );
+    const text = await response.text();
+    expect(text).toBe('Hello, world!');
+  });
+});
+
