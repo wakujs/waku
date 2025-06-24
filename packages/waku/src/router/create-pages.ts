8aa14fb6394a9b3973c6ce515f1686ef4f044e87
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-06-24 10:23:22 -0400

    Format fix

--- packages/waku/src/router/create-pages.ts
+++ packages/waku/src/router/create-pages.ts
@@ -753,16 +753,19 @@ export const createPages = <
           };
         },
       );
-      return [...routeConfigs, ...apiConfigs] // Sort wildcard routes to the end - we want to match the most specific routes first
-        .sort((pathConfigA, pathConfigB) => {
-          if (pathConfigA.path.find((spec) => spec.type === 'wildcard')) {
-            return 1;
-          }
-          if (pathConfigB.path.find((spec) => spec.type === 'wildcard')) {
-            return -1;
-          }
-          return 0;
-        });
+      return (
+        [...routeConfigs, ...apiConfigs]
+          // Sort wildcard routes to the end - we want to match the most specific routes first
+          .sort((pathConfigA, pathConfigB) => {
+            if (pathConfigA.path.find((spec) => spec.type === 'wildcard')) {
+              return 1;
+            }
+            if (pathConfigB.path.find((spec) => spec.type === 'wildcard')) {
+              return -1;
+            }
+            return 0;
+          })
+      );
     },
     handleRoute: async (path, { query }) => {
       await configure();
