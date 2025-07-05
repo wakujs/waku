18128a6e9e280e25b1f5b1f745cf440b72c8a338
Matt Hamlin <matthewjameshamlin@gmail.com>
2025-07-04 18:37:18 -0700

    Add e2e test cases for both issues
    - catch-all route not handling index route
    - catch-all api route incorrectly matching before standard api route

--- e2e/wildcard-api-routes.spec.ts
+++ e2e/wildcard-api-routes.spec.ts
@@ -14,17 +14,22 @@ test.describe(`wildcard api routes`, async () => {
     await stopApp();
   });
 
+  test('catch all route can match as index route', async ({ page }) => {
+    await page.goto(`http://localhost:${port}/`);
+    await expect(page.getByText('Catch All Pages Route')).toBeVisible();
+  });
+
   test(`api route matches before wildcard route`, async ({ page }) => {
-    // index route matches wildcard:
-    await page.goto(`http://localhost:${port}/foo`);
-    await expect(page.getByRole('heading', { name: '/foo' })).toBeVisible();
+    // misc route matches wildcard:
+    await page.goto(`http://localhost:${port}/foo/bar`);
+    await expect(page.getByRole('heading', { name: '/foo/bar' })).toBeVisible();
 
     // api route request
     const response = await page.request.get(
       `http://localhost:${port}/api/greet`,
     );
     const text = await response.text();
-    expect(text).toBe('Hello, world!');
+    expect(text).toBe('Greetings from the API!');
   });
 
   test(`api wildcard route matches before standard wildcard route`, async ({
@@ -35,5 +40,11 @@ test.describe(`wildcard api routes`, async () => {
     );
     const text = await response.text();
     expect(text).toBe('API Wildcard!');
+
+    const response2 = await page.request.get(
+      `http://localhost:${port}/api/foo/bar`,
+    );
+    const text2 = await response2.text();
+    expect(text2).toBe('/api root catch-all');
   });
 });
