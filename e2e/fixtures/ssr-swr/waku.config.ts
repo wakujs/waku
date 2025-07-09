19052a2b329bd60fc045dd9d5daf5174a4f1d226
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-09 15:23:35 +0900

    chore: reduce diff

--- e2e/fixtures/ssr-swr/waku.config.ts
+++ e2e/fixtures/ssr-swr/waku.config.ts
@@ -3,11 +3,6 @@ import { defineConfig } from 'waku/config';
 export default defineConfig({
   vite: {
     environments: {
-      client: {
-        optimizeDeps: {
-          include: ['swr'],
-        },
-      },
       ssr: {
         optimizeDeps: {
           include: ['swr'],
