13d05d5597e066c8d8d10c8f393f011a2c7a3e9e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-09 12:04:59 +0900

    chore: lint

--- packages/create-waku/src/tests/cli-with-args.test.ts
+++ packages/create-waku/src/tests/cli-with-args.test.ts
@@ -2,7 +2,14 @@ import fs from 'node:fs';
 import path from 'node:path';
 import type { SyncOptions, SyncResult } from 'execa';
 import { execaCommandSync } from 'execa';
-import { afterEach, beforeAll, describe, expect, onTestFailed, test } from 'vitest';
+import {
+  afterEach,
+  beforeAll,
+  describe,
+  expect,
+  onTestFailed,
+  test,
+} from 'vitest';
 
 const CLI_PATH = path.join(import.meta.dirname, '../../dist/index.js');
 
@@ -21,11 +28,11 @@ const run = <SO extends SyncOptions>(
   const command = `node ${CLI_PATH} ${args.join(' ')}`;
   const result = execaCommandSync(command, options);
   onTestFailed(() => {
-    console.error("======= CLI:", command)
-    console.error("======= stdout")
-    console.error(result.stdout)
-    console.error("======= stderr")
-    console.error(result.stderr)
+    console.error('======= CLI:', command);
+    console.error('======= stdout');
+    console.error(result.stdout);
+    console.error('======= stderr');
+    console.error(result.stderr);
   });
   // @ts-expect-error relies on exactOptionalPropertyTypes being false
   return result;
