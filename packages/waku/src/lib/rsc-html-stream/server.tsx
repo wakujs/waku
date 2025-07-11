bef7b0f3e332717eb1d538e0e5335337d84d6733
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-11 18:16:53 +0900

    chore: lint

--- packages/waku/src/lib/rsc-html-stream/server.tsx
+++ packages/waku/src/lib/rsc-html-stream/server.tsx
@@ -7,9 +7,12 @@ const trailer = '</body></html>';
 export function injectRSCPayload(rscStream, options = {}) {
   let decoder = new TextDecoder();
   let resolveFlightDataPromise;
-  let flightDataPromise = new Promise((resolve) => resolveFlightDataPromise = resolve);
+  let flightDataPromise = new Promise(
+    (resolve) => (resolveFlightDataPromise = resolve),
+  );
   let startedRSC = false;
-  let nonce = options && typeof options.nonce === 'string' ? options.nonce : undefined;
+  let nonce =
+    options && typeof options.nonce === 'string' ? options.nonce : undefined;
 
   // Buffer all HTML chunks enqueued during the current tick of the event loop (roughly)
   // and write them to the output stream all at once. This ensures that we don't generate
@@ -18,7 +21,7 @@ export function injectRSCPayload(rscStream, options = {}) {
   let timeout = null;
   function flushBufferedChunks(controller) {
     for (let chunk of buffered) {
-      let buf = decoder.decode(chunk, {stream: true});
+      let buf = decoder.decode(chunk, { stream: true });
       if (buf.endsWith(trailer)) {
         buf = buf.slice(0, -trailer.length);
       }
@@ -51,7 +54,7 @@ export function injectRSCPayload(rscStream, options = {}) {
         if (!startedRSC) {
           startedRSC = true;
           writeRSCStream(rscStream, controller, nonce)
-            .catch(err => controller.error(err))
+            .catch((err) => controller.error(err))
             .then(resolveFlightDataPromise);
         }
       }, 0);
@@ -63,20 +66,28 @@ export function injectRSCPayload(rscStream, options = {}) {
         flushBufferedChunks(controller);
       }
       controller.enqueue(encoder.encode(trailer));
-    }
+    },
   });
 }
 
 async function writeRSCStream(rscStream, controller, nonce) {
-  let decoder = new TextDecoder('utf-8', {fatal: true});
+  let decoder = new TextDecoder('utf-8', { fatal: true });
   for await (let chunk of rscStream) {
     // Try decoding the chunk to send as a string.
     // If that fails (e.g. binary data that is invalid unicode), write as base64.
     try {
-      writeChunk(JSON.stringify(decoder.decode(chunk, {stream: true})), controller, nonce);
+      writeChunk(
+        JSON.stringify(decoder.decode(chunk, { stream: true })),
+        controller,
+        nonce,
+      );
     } catch (err) {
       let base64 = JSON.stringify(btoa(String.fromCodePoint(...chunk)));
-      writeChunk(`Uint8Array.from(atob(${base64}), m => m.codePointAt(0))`, controller, nonce);
+      writeChunk(
+        `Uint8Array.from(atob(${base64}), m => m.codePointAt(0))`,
+        controller,
+        nonce,
+      );
     }
   }
 
@@ -87,7 +98,11 @@ async function writeRSCStream(rscStream, controller, nonce) {
 }
 
 function writeChunk(chunk, controller, nonce) {
-  controller.enqueue(encoder.encode(`<script${nonce ? ` nonce="${nonce}"` : ""}>${escapeScript(`(self.__FLIGHT_DATA||=[]).push(${chunk})`)}</script>`));
+  controller.enqueue(
+    encoder.encode(
+      `<script${nonce ? ` nonce="${nonce}"` : ''}>${escapeScript(`(self.__FLIGHT_DATA||=[]).push(${chunk})`)}</script>`,
+    ),
+  );
 }
 
 // Escape closing script tags and HTML comments in JS content.
@@ -95,7 +110,5 @@ function writeChunk(chunk, controller, nonce) {
 // Avoid replacing </script with <\/script as it would break the following valid JS: 0</script/ (i.e. regexp literal).
 // Instead, escape the s character.
 function escapeScript(script) {
-  return script
-    .replace(/<!--/g, '<\\!--')
-    .replace(/<\/(script)/gi, '</\\$1');
+  return script.replace(/<!--/g, '<\\!--').replace(/<\/(script)/gi, '</\\$1');
 }
