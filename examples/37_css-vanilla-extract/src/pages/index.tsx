2efcea3c548f1d65c3c6d2537e2255ebc02aa59a
daishi <daishi@axlight.com>
2025-07-14 21:08:09 +0900

    e2e: recover smoke tests

--- examples/37_css-vanilla-extract/src/pages/index.tsx
+++ examples/37_css-vanilla-extract/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { serverStyle } from '../server.css';
 export default async function HomePage() {
   return (
     <div>
+      <title>Waku</title>
       <div className={serverStyle}>Server Style (green)</div>
       <div>
         <Counter />
