572424bc0bb8912db412b8fa5ecfc3298dfa69b6
daishi <daishi@axlight.com>
2025-07-21 14:37:56 +0900

    fix format

--- e2e/fixtures/ssr-basic/src/components/test-app.tsx
+++ e2e/fixtures/ssr-basic/src/components/test-app.tsx
@@ -1,6 +1,6 @@
 // https://github.com/wakujs/waku/pull/1539
 
-import { TestClient } from "./test-client.js";
+import { TestClient } from './test-client.js';
 
 const TestApp = () => {
   return (
@@ -10,7 +10,7 @@ const TestApp = () => {
       </head>
       <body>
         <div>
-          <TestClient serverPromise={Promise.resolve("test")}/>
+          <TestClient serverPromise={Promise.resolve('test')} />
         </div>
       </body>
     </html>
