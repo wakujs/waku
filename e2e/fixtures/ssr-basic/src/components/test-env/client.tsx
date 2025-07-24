6e61625c46178338658ec252be2192b20b0a8401
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-07-24 09:46:51 +0900

    chore: prettier

--- e2e/fixtures/ssr-basic/src/components/test-env/client.tsx
+++ e2e/fixtures/ssr-basic/src/components/test-env/client.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 const data = {
-  "import.meta.env.WAKU_PUBLIC_TEST": import.meta.env.WAKU_PUBLIC_TEST || '-',
-  "import.meta.env.WAKU_PRIVATE_TEST": import.meta.env.WAKU_PRIVATE_TEST || '-',
-  "process.env.WAKU_PUBLIC_TEST": process.env.WAKU_PUBLIC_TEST || '-',
+  'import.meta.env.WAKU_PUBLIC_TEST': import.meta.env.WAKU_PUBLIC_TEST || '-',
+  'import.meta.env.WAKU_PRIVATE_TEST': import.meta.env.WAKU_PRIVATE_TEST || '-',
+  'process.env.WAKU_PUBLIC_TEST': process.env.WAKU_PUBLIC_TEST || '-',
   // this is skipped since SSR can access process.env and cause hydration error
   // "process.env.WAKU_PRIVATE_TEST": process.env.WAKU_PRIVATE_TEST || '-',
 };
--- e2e/fixtures/ssr-basic/src/components/test-env/server.tsx
+++ e2e/fixtures/ssr-basic/src/components/test-env/server.tsx
@@ -1,9 +1,10 @@
 export function TestEnvServer() {
   const data = {
-    "import.meta.env.WAKU_PUBLIC_TEST": import.meta.env.WAKU_PUBLIC_TEST || '-',
-    "import.meta.env.WAKU_PRIVATE_TEST": import.meta.env.WAKU_PRIVATE_TEST || '-',
-    "process.env.WAKU_PUBLIC_TEST": process.env.WAKU_PUBLIC_TEST || '-',
-    "process.env.WAKU_PRIVATE_TEST": process.env.WAKU_PRIVATE_TEST || '-',
+    'import.meta.env.WAKU_PUBLIC_TEST': import.meta.env.WAKU_PUBLIC_TEST || '-',
+    'import.meta.env.WAKU_PRIVATE_TEST':
+      import.meta.env.WAKU_PRIVATE_TEST || '-',
+    'process.env.WAKU_PUBLIC_TEST': process.env.WAKU_PUBLIC_TEST || '-',
+    'process.env.WAKU_PRIVATE_TEST': process.env.WAKU_PRIVATE_TEST || '-',
   };
   return <pre>{JSON.stringify(data, null, 2)}</pre>;
 }
