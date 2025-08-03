73dde6ea787abde28a32466eb8963f72c5f76767
Tyler <26290074+tylersayshi@users.noreply.github.com>
2025-08-03 15:38:37 -0700

    fix example

--- examples/11_fs-router/src/pages/slice-page.tsx
+++ examples/11_fs-router/src/pages/slice-page.tsx
@@ -4,8 +4,8 @@ export default function SlicePage() {
   return (
     <div>
       <h2>Slice Page</h2>
-      <Slice id="slice-one" />
-      <Slice id="slice-two" />
+      <Slice id="one" />
+      <Slice id="two" />
     </div>
   );
 }
