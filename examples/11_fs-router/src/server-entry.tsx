1fb622273cffb1df4a1dfb3757f1ce77c5dbeebb
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-06 09:57:17 +0900

    chore: update examples

--- examples/11_fs-router/src/server-entry.tsx
+++ examples/11_fs-router/src/server-entry.tsx
@@ -2,6 +2,6 @@
 import { unstable_fsRouter as fsRouter } from 'waku/router/server';
 
 export default fsRouter(
-  import.meta.glob('./pages/**/*.tsx', { base: "./pages" }),
+  import.meta.glob('/src/pages/**/*.tsx', { base: "/src/pages" }),
   { apiDir: 'api' },
 )
