7534331c25ea84a81daed59830868b69fc6f3d92
Rob Marscher <rob@robmarscher.com>
2025-08-11 11:02:08 -0400

    feat: export createApp

--- packages/waku/src/vite-rsc/lib/engine.ts
+++ packages/waku/src/vite-rsc/lib/engine.ts
@@ -3,8 +3,14 @@ import type {
   MiddlewareOptions,
 } from '../../lib/middleware/types.js';
 import { middlewares } from 'virtual:vite-rsc-waku/middlewares';
-import type { MiddlewareHandler } from 'hono';
-import { isBuild } from 'virtual:vite-rsc-waku/config';
+import type { MiddlewareHandler, Hono } from 'hono';
+import { flags, config, isBuild } from 'virtual:vite-rsc-waku/config';
+import { compress } from 'hono/compress';
+import { serveStatic } from '@hono/node-server/serve-static';
+import path from 'node:path';
+import fs from 'node:fs';
+import { DIST_PUBLIC } from '../../lib/builder/constants.js';
+import { INTERNAL_setAllEnv } from '../../server.js';
 
 // cf. packages/waku/src/lib/hono/engine.ts
 export function createHonoHandler(): MiddlewareHandler {
@@ -67,4 +73,24 @@ export function createHonoHandler(): MiddlewareHandler {
     }
     await next();
   };
+}
+
+// cf. packages/waku/src/vite-rsc/entry.server.tsx
+export function createApp(app: Hono) {
+  INTERNAL_setAllEnv(process.env as any);
+  if (flags['experimental-compress']) {
+    app.use(compress());
+  }
+  if (isBuild) {
+    app.use(serveStatic({ root: path.join(config.distDir, DIST_PUBLIC) }));
+  }
+  app.use(createHonoHandler());
+  app.notFound((c) => {
+    const file = path.join(config.distDir, DIST_PUBLIC, '404.html');
+    if (fs.existsSync(file)) {
+      return c.html(fs.readFileSync(file, 'utf8'), 404);
+    }
+    return c.text('404 Not Found', 404);
+  });
+  return app;
 }
