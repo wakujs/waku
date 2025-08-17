4698ab57577bada2dd350f4ee8cae075d7a425a3
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-17 18:03:55 +0900

    chore: lint

--- e2e/fixtures/render-type/src/build/dynamic.tsx
+++ e2e/fixtures/render-type/src/build/dynamic.tsx
@@ -3,7 +3,10 @@ import { unstable_getBuildOptions } from 'waku/server';
 export default function Dynamic() {
   return (
     <div>
-      [dynamic] phase = <span data-testid="phase">{String(!!unstable_getBuildOptions().unstable_phase)}</span>
+      [dynamic] phase ={' '}
+      <span data-testid="phase">
+        {String(!!unstable_getBuildOptions().unstable_phase)}
+      </span>
     </div>
   );
 }
--- e2e/fixtures/render-type/src/build/static.tsx
+++ e2e/fixtures/render-type/src/build/static.tsx
@@ -3,7 +3,10 @@ import { unstable_getBuildOptions } from 'waku/server';
 export default function Static() {
   return (
     <div>
-      [static] phase = <span data-testid="phase">{String(!!unstable_getBuildOptions().unstable_phase)}</span>
+      [static] phase ={' '}
+      <span data-testid="phase">
+        {String(!!unstable_getBuildOptions().unstable_phase)}
+      </span>
     </div>
   );
 }
