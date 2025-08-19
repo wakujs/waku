a8ca3650aafa3c700a8204ec7c535fad3d99530e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-19 09:08:13 +0900

    chore: fix fixme

--- e2e/fixtures/ssr-basic/waku.config.ts
+++ e2e/fixtures/ssr-basic/waku.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'waku/config';
 
 export default defineConfig({
   vite: {
-    // FIXME How can we limit this config for only mode=development?
     optimizeDeps: {
       exclude: ['@ai-sdk/rsc'],
     },
