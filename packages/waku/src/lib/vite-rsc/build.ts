6c3e4537ce900fc7c69c428f8c4eaa1538e6aae7
Daishi Kato <dai-shi@users.noreply.github.com>
2025-08-19 23:25:59 +0900

    refactor: request/render utils (#1638)
    - and some cleanups

--- packages/waku/src/lib/vite-rsc/build.ts
+++ packages/waku/src/lib/vite-rsc/build.ts
@@ -1,11 +1,16 @@
-import { createRenderUtils } from './render.js';
+import { renderToReadableStream } from '@vitejs/plugin-rsc/rsc';
+import { createRenderUtils } from '../utils/render.js';
 import { encodeRscPath } from '../renderers/utils.js';
 import { joinPath } from '../utils/path.js';
 import { config } from 'virtual:vite-rsc-waku/config';
 import serverEntry from 'virtual:vite-rsc-waku/server-entry';
 
 export async function handleBuild() {
-  const renderUtils = createRenderUtils({});
+  const renderUtils = createRenderUtils(
+    undefined,
+    renderToReadableStream,
+    loadSsrEntryModule,
+  );
 
   const buidlResult = serverEntry.handleBuild({
     renderRsc: renderUtils.renderRsc,
@@ -23,4 +28,13 @@ export async function handleBuild() {
   });
 
   return buidlResult;
+}
+
+function loadSsrEntryModule() {
+  // This is an API to communicate between two server environments `rsc` and `ssr`.
+  // https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-rsc/README.md#importmetaviterscloadmodule
+  return import.meta.viteRsc.loadModule<typeof import('./ssr.js')>(
+    'ssr',
+    'index',
+  );
 }
