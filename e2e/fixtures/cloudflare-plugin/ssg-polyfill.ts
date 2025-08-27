801cefbd341d2d82e10108e80717dfa5d4759a04
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-08-27 13:02:11 +0900

    chore: avoid sync hook

--- e2e/fixtures/cloudflare-plugin/ssg-polyfill.ts
+++ e2e/fixtures/cloudflare-plugin/ssg-polyfill.ts
@@ -1,8 +1,6 @@
 import { Plugin } from 'vite';
-import { registerHooks } from 'node:module';
+import { LoadHook, register, ResolveHook } from 'node:module';
 import { getPlatformProxy } from 'wrangler';
-
-// polyfill "cloudflare:workers" for ssg running on Node
 
 export function ssgPolyfillPlugin(): Plugin[] {
   let deregister: () => Promise<void>;
@@ -26,34 +24,42 @@ export function ssgPolyfillPlugin(): Plugin[] {
   ];
 }
 
+// use node custom loader to implement "cloudflare:workers"
 async function registerPolyfill() {
   const platformProxy = await getPlatformProxy();
   (globalThis as any).__polyfill_platform_proxy = platformProxy;
 
-  const hooks = registerHooks({
-    resolve: (specifier, context, nextResolve) => {
-      if (specifier === 'cloudflare:workers') {
-        return {
-          shortCircuit: true,
-          url: specifier,
-        };
-      }
-      return nextResolve(specifier, context);
-    },
-    load: (url, context, nextLoad) => {
-      if (url === 'cloudflare:workers') {
-        return {
-          shortCircuit: true,
-          format: 'module',
-          source: `export const env = globalThis.__polyfill_platform_proxy.env`,
-        };
-      }
-      return nextLoad(url, context);
-    },
-  });
+  const resolveFn: ResolveHook = async function (
+    specifier,
+    context,
+    nextResolve,
+  ) {
+    if (specifier === 'cloudflare:workers') {
+      return {
+        shortCircuit: true,
+        url: specifier,
+      };
+    }
+    return nextResolve(specifier, context);
+  };
+
+  const loadFn: LoadHook = async function (url, context, nextLoad) {
+    if (url === 'cloudflare:workers') {
+      return {
+        shortCircuit: true,
+        format: 'module',
+        source: `export const env = globalThis.__polyfill_platform_proxy.env`,
+      };
+    }
+    return nextLoad(url, context);
+  };
+
+  register(`data:text/javascript,
+export const resolve = ${resolveFn.toString()};
+export const load = ${loadFn.toString()};    
+`);
 
   return async () => {
     await platformProxy.dispose();
-    hooks.deregister();
   };
 }
