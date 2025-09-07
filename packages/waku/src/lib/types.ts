3ad94732ed6807527ff1b1312d1cda8b4390f2cf
daishi <daishi@axlight.com>
2025-09-07 15:46:09 +0900

    ideating types

--- packages/waku/src/lib/types.ts
+++ packages/waku/src/lib/types.ts
@@ -2,6 +2,20 @@ import type { ReactNode } from 'react';
 
 type Elements = Record<string, unknown>;
 
+type GetInput = () =>
+  | { type: 'component'; rscPath: string; rscParams: unknown }
+  | {
+      type: 'function';
+      fn: (...args: unknown[]) => Promise<unknown>;
+      args: unknown[];
+    }
+  | {
+      type: 'action';
+      fn: () => Promise<unknown>;
+      pathname: string;
+    }
+  | { type: 'custom'; pathname: string };
+
 type RenderRsc = (elements: Record<string, unknown>) => Promise<ReadableStream>;
 
 type RenderHtml = (
@@ -53,6 +67,7 @@ export type HandleBuild = (utils: {
   rscPath2pathname: (rscPath: string) => string;
 }) => AsyncIterable<BuildConfig> | null;
 
+/** @deprecated This will be removed soon. */
 export type ServerEntries = {
   default: {
     handleRequest: HandleRequest;
@@ -78,3 +93,32 @@ export type MiddlewareOptions = {
 
 /** @deprecated This will be removed soon. */
 export type Middleware = (options: MiddlewareOptions) => Handler;
+
+export type unstable_ServerEntryFetch = (
+  req: Request,
+  utils: {
+    getInput: GetInput;
+    renderRsc: RenderRsc;
+    renderHtml: RenderHtml;
+    renderDefaultHtml: () => Promise<Response>;
+    renderNotFound: () => Promise<Response>;
+  },
+) => Promise<Response>;
+
+export type unstable_ServerEntryBuild = (utils: {
+  renderRsc: RenderRsc;
+  renderHtml: RenderHtml;
+  rscPath2pathname: (rscPath: string) => string;
+  generateFile: (
+    pathname: string,
+    body: ReadableStream | string,
+  ) => Promise<void>;
+  generateDefaultHtml: (pathname: string) => Promise<void>;
+}) => Promise<void>;
+
+export type unstable_ServerEntry = {
+  default: {
+    fetch: unstable_ServerEntryFetch;
+    build: unstable_ServerEntryBuild;
+  };
+};
--- packages/waku/src/minimal/server.ts
+++ packages/waku/src/minimal/server.ts
@@ -1,5 +1,12 @@
-import type { ServerEntries } from '../lib/types.js';
+import type {
+  ServerEntries,
+  unstable_ServerEntry as ServerEntry,
+} from '../lib/types.js';
 
 export function unstable_defineEntries(fns: ServerEntries['default']) {
+  return fns;
+}
+
+export function unstable_defineServer(fns: ServerEntry['default']) {
   return fns;
 }
