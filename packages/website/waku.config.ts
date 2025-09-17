722554d3d42d4d4c3b284d4e30bede787e05d5be
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-17 19:10:06 +0900

    fix(website): use nitro plugin to add custom vercel routes

--- packages/website/waku.config.ts
+++ packages/website/waku.config.ts
@@ -1,9 +1,43 @@
 import { defineConfig } from 'waku/config';
 import tailwindcss from '@tailwindcss/vite';
+import nitro from '@hiogawa/vite-plugin-nitro';
+
+// prevent waku's automatic --with-vercel build
+delete process.env.VERCEL;
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      nitro({
+        server: {
+          environmentName: 'rsc',
+        },
+        config: {
+          preset: 'vercel-static',
+          vercel: {
+            config: {
+              // https://vercel.com/docs/build-output-api/configuration#routing-rule-example
+              routes: [
+                {
+                  src: '/redirect',
+                  status: 308,
+                  headers: {
+                    Location: 'https://example.com/',
+                  },
+                },
+                {
+                  src: '/RSC/(.*)',
+                  headers: {
+                    'X-Robots-Tag': 'noindex',
+                  },
+                },
+              ] as any, // TODO: nitro type issue?
+            },
+          },
+        },
+      }),
+    ],
     environments: {
       rsc: {
         resolve: {
