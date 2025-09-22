ba17e21e688ff659713c58b17069db6623989b07
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-22 13:29:31 +0900

    chore: cleanup

--- examples/01_template/start-static.js
+++ examples/01_template/start-static.js
@@ -6,6 +6,11 @@ const base = "/custom-base/";
 const server = http.createServer((req, res) => {
   if (req.url.startsWith(base)) {
     req.url = req.url.slice(base.length - 1);
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end(`<a href="${base}">Return to base</a>`);
+    return;
   }
   return serveHandler(req, res, {
     public: 'dist/public',
@@ -13,5 +18,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8080, () => {
-  console.log(`ready: Listening on http://localhost:3000${base}`);
+  console.log(`ready: Listening on http://localhost:8080${base}`);
 });
