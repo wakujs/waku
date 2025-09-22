da010ef38b0a8c36c99073495c269dba80f2bc82
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-22 13:24:45 +0900

    chore: add start-static example

---
+++ examples/01_template/start-static.js
@@ -0,0 +1,17 @@
+import http from 'node:http'
+import serveHandler from 'serve-handler'
+
+const base = "/custom-base/";
+
+const server = http.createServer((req, res) => {
+  if (req.url.startsWith(base)) {
+    req.url = req.url.slice(base.length - 1);
+  }
+  return serveHandler(req, res, {
+    public: 'dist/public',
+  });
+});
+
+server.listen(8080, () => {
+  console.log(`ready: Listening on http://localhost:3000${base}`);
+});
