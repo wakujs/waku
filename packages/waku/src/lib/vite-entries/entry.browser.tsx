8f5b5ef92b170ae4c104643637f8c7ca4d6eb6f1
Daishi Kato <dai-shi@users.noreply.github.com>
2025-09-27 11:13:01 +0900

    refactor: browser preamble (#1708)
    Does this work?

--- packages/waku/src/lib/vite-entries/entry.browser.tsx
+++ packages/waku/src/lib/vite-entries/entry.browser.tsx
@@ -1,2 +1,12 @@
-import '../vite-rsc/browser-preamble.js';
+import { setServerCallback } from '@vitejs/plugin-rsc/browser';
+import { unstable_callServerRsc } from '../../minimal/client.js';
+setServerCallback(unstable_callServerRsc);
+
+if (import.meta.hot) {
+  import.meta.hot.on('rsc:update', (e) => {
+    console.log('[rsc:update]', e);
+    globalThis.__WAKU_RSC_RELOAD_LISTENERS__?.forEach((l) => l());
+  });
+}
+
 import 'virtual:vite-rsc-waku/client-entry';
