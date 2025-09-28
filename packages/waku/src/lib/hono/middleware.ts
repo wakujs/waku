b8e49fc10d3c8d91c1574ec6ad2e1b1820235bb1
daishi <daishi@axlight.com>
2025-09-29 08:45:13 +0900

    combined middleware file

--- packages/waku/src/lib/hono/middleware.ts
+++ packages/waku/src/lib/hono/middleware.ts
@@ -1,5 +1,51 @@
-export { contextMiddleware } from './middleware/context.js';
-export { staticMiddleware } from './middleware/static.js';
-export { rscMiddleware } from './middleware/rsc.js';
-export { notFoundMiddleware } from './middleware/not-found.js';
-export { middlewareRunner } from './middleware/runner.js';
+import type { MiddlewareHandler } from 'hono';
+import { INTERNAL_runWithContext } from '../context.js';
+import type { Unstable_ProcessRequest as ProcessRequest } from '../types.js';
+
+export function contextMiddleware(): MiddlewareHandler {
+  return (c, next) => {
+    const req = c.req.raw;
+    return INTERNAL_runWithContext(req, next);
+  };
+}
+
+export function rscMiddleware({
+  processRequest,
+}: {
+  processRequest: ProcessRequest;
+}): MiddlewareHandler {
+  return async (c, next) => {
+    const req = c.req.raw;
+    const res = await processRequest(req);
+    if (res) {
+      c.res = res;
+      return;
+    }
+    await next();
+  };
+}
+
+export function middlewareRunner(
+  middlewareModules: Record<
+    string,
+    () => Promise<{
+      default: () => MiddlewareHandler;
+    }>
+  >,
+): MiddlewareHandler {
+  let handlers: MiddlewareHandler[] | undefined;
+  return async (c, next) => {
+    if (!handlers) {
+      handlers = await Promise.all(
+        Object.values(middlewareModules).map((m) =>
+          m().then((mod) => mod.default()),
+        ),
+      );
+    }
+    const run = async (index: number) => {
+      await handlers![index]?.(c, () => run(index + 1));
+    };
+    await run(0);
+    await next();
+  };
+}
