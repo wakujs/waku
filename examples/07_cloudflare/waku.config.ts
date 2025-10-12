14eebdb9ed412fe605a8f9f7d592c2590d6aec02
daishi <daishi@axlight.com>
2025-10-12 13:39:08 +0900

    Revert "does this help?"
    This reverts commit 1a7bc076f1b4a6668ff904f51a53ea2eed58bb1f.

--- examples/07_cloudflare/waku.config.ts
+++ examples/07_cloudflare/waku.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'waku/config';
 import tailwindcss from '@tailwindcss/vite';
-import type { PluginOption } from 'vite';
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss() as PluginOption],
+    plugins: [tailwindcss()],
     optimizeDeps: {
       exclude: ['sqlite'],
     },
