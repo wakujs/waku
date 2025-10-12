f49ab4ff9ba50a5217e7a23f3ac6ae12badfb4c5
Daishi Kato <dai-shi@users.noreply.github.com>
2025-10-12 13:42:28 +0900

    fix: override NODE_ENV to avoid confusion (#1721)
    follow
    https://github.com/wakujs/waku/issues/1713#issuecomment-3349641450 and
    close #1713.

--- packages/waku/src/lib/vite-rsc/cli.ts
+++ packages/waku/src/lib/vite-rsc/cli.ts
@@ -58,7 +58,13 @@ export async function cli(
   flags: { host?: string; port?: string } & Flags,
 ) {
   // set NODE_ENV before runnerImport https://github.com/vitejs/vite/issues/20299
-  process.env.NODE_ENV ??= cmd === 'dev' ? 'development' : 'production';
+  const nodeEnv = cmd === 'dev' ? 'development' : 'production';
+  if (process.env.NODE_ENV && process.env.NODE_ENV !== nodeEnv) {
+    console.warn(
+      `Warning: NODE_ENV is set to '${process.env.NODE_ENV}', but overriding it to '${nodeEnv}'.`,
+    );
+  }
+  process.env.NODE_ENV = nodeEnv;
 
   const rscPluginOptions: RscPluginOptions = {
     flags,
