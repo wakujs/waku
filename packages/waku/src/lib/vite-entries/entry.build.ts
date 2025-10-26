df21ec19474b2b446f316d0728287b2652e92ae8
daishi <daishi@axlight.com>
2025-10-26 14:11:39 +0900

    fix import on windows

--- packages/waku/src/lib/vite-entries/entry.build.ts
+++ packages/waku/src/lib/vite-entries/entry.build.ts
@@ -1,6 +1,8 @@
-import { pathToFileURL } from 'node:url';
 import serverEntry from 'virtual:vite-rsc-waku/server-entry';
 import { INTERNAL_setAllEnv } from '../../server.js';
+import { filePathToFileURL } from '../utils/path.js';
+
+const WIN32_PATH_REGEXP = /^[a-zA-Z]:\//;
 
 export async function INTERNAL_runBuild({
   savePlatformData,
@@ -12,9 +14,12 @@ export async function INTERNAL_runBuild({
   await savePlatformData();
   if (serverEntry.postBuild) {
     const [modulePath, ...args] = serverEntry.postBuild;
-    const mod = await import(
-      modulePath.startsWith('/') ? pathToFileURL(modulePath).href : modulePath
-    );
+    const moduleId = modulePath.match(WIN32_PATH_REGEXP)
+      ? filePathToFileURL('/' + modulePath)
+      : modulePath.startsWith('/')
+        ? filePathToFileURL(modulePath)
+        : modulePath;
+    const mod = await import(moduleId);
     mod.default(...args);
   }
 }
