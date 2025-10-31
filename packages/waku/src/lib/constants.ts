fe8fb224abbaba0fa962782a25690264945e1114
Daishi Kato <dai-shi@users.noreply.github.com>
2025-10-31 18:19:51 +0900

    fix(adapters): vercel adapter with assetsDir option (#1742)
    https://github.com/wakujs/waku/issues/1741#issuecomment-3465986848
    
    and remove unused constants.

--- packages/waku/src/lib/constants.ts
+++ packages/waku/src/lib/constants.ts
@@ -5,8 +5,4 @@ export const SRC_PAGES = 'pages'; // only for managed mode
 
 // Some file and dir names for dist
 // We may change this in the future
-export const DIST_SERVER_ENTRY_JS = 'server-entry.js';
-export const DIST_SERVE_JS = 'serve.js';
 export const DIST_PUBLIC = 'public';
-export const DIST_ASSETS = 'assets';
-export const DIST_SSR = 'ssr';
