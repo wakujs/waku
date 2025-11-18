9b0f9c60f346d97a44b9bd430555bf74cabe4156
Rob Marscher <rob@robmarscher.com>
2025-11-18 10:46:30 -0500

    Add cloudflare to default adapter

--- packages/waku/src/adapters/default.ts
+++ packages/waku/src/adapters/default.ts
@@ -1,12 +1,15 @@
 // This file won't be used actually as it will be replaced by the build process.
 // See src/lib/vite-rsc/plugin.ts (createVirtualAdapterPlugin)
 
+import cloudflareAdapter from './cloudflare.js';
 import netlifyAdapter from './netlify.js';
 import nodeAdapter from './node.js';
 import vercelAdapter from './vercel.js';
 
-export default process.env.VERCEL
+export default
   ? vercelAdapter
   : process.env.NETLIFY
-    ? netlifyAdapter
+      ? netlifyAdapter
+      : process.env.CLOUDFLARE
+    ? cloudflareAdapter
     : nodeAdapter;
