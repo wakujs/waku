8c968386306440e1dcd4f3666e27925b94b5eb7c
daishi <daishi@axlight.com>
2025-11-30 16:37:57 +0900

    fix format

--- examples/11_fs-router/src/server-entry.tsx
+++ examples/11_fs-router/src/server-entry.tsx
@@ -3,7 +3,9 @@
 import { fsRouter } from 'waku';
 import adapter from 'waku/adapters/default';
 
-const router = fsRouter(import.meta.glob('./**/*.{tsx,ts}', { base: './pages' }))
+const router = fsRouter(
+  import.meta.glob('./**/*.{tsx,ts}', { base: './pages' }),
+);
 
 export const getRouterConfigs = () => router.unstable_getRouterConfigs();
 
