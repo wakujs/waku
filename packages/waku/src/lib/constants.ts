f64ef849e01846be7e87576b5f896487196a74c0
daishi <daishi@axlight.com>
2025-12-01 14:34:53 +0900

    refactor with a constant

--- packages/waku/src/lib/constants.ts
+++ packages/waku/src/lib/constants.ts
@@ -2,6 +2,7 @@ export const EXTENSIONS = ['.js', '.ts', '.tsx', '.jsx', '.mjs', '.cjs'];
 export const SRC_CLIENT_ENTRY = 'client-entry';
 export const SRC_SERVER_ENTRY = 'server-entry';
 export const SRC_PAGES = 'pages'; // only for managed mode
+export const SRC_MIDDLEWARE = 'middleware'; // only for managed mode
 
 // Some file and dir names for dist
 // We may change this in the future
