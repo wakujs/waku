9e42ec5bc26ea6ba8bff1d55092434373d2a44b2
daishi <daishi@axlight.com>
2025-12-03 16:00:57 +0900

    hack website server entry

--- packages/website/src/server-entry.tsx
+++ packages/website/src/server-entry.tsx
@@ -1,5 +1,10 @@
 import { fsRouter } from 'waku';
-import adapter from 'waku/adapters/vercel';
+import nodeAdapter from 'waku/adapters/node';
+import vercelAdapter from 'waku/adapters/vercel';
+
+const adapter: typeof vercelAdapter = process.env.VERCEL
+  ? vercelAdapter
+  : nodeAdapter;
 
 const handler: ReturnType<typeof adapter> = adapter(
   fsRouter(import.meta.glob('./**/*.{tsx,ts}', { base: './pages' })),
