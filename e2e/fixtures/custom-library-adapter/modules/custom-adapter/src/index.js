f9c041d3eae3c10c0e89784b1e7f3f1e40382705
daishi <daishi@axlight.com>
2025-12-15 17:45:32 +0900

    fix: lint

--- e2e/fixtures/custom-library-adapter/modules/custom-adapter/src/index.js
+++ e2e/fixtures/custom-library-adapter/modules/custom-adapter/src/index.js
@@ -1,15 +1,17 @@
 import { unstable_createServerEntryAdapter as createServerEntryAdapter } from 'waku/internals';
 
-export default createServerEntryAdapter(({ processRequest, processBuild, config }) => {
-  return {
-    async fetch(req, ...args) {
-      const res = await processRequest(req, ...args);
-      return res ?? new Response('Not Found', { status: 404 });
-    },
-    build: processBuild,
-    postBuild: [
-      new URL('./post-build.js', import.meta.url).href,
-      { distDir: config.distDir, marker: 'custom-adapter-post-build' },
-    ],
-  };
-});
+export default createServerEntryAdapter(
+  ({ processRequest, processBuild, config }) => {
+    return {
+      async fetch(req) {
+        const res = await processRequest(req);
+        return res ?? new Response('Not Found', { status: 404 });
+      },
+      build: processBuild,
+      postBuild: [
+        new URL('./post-build.js', import.meta.url).href,
+        { distDir: config.distDir, marker: 'custom-adapter-post-build' },
+      ],
+    };
+  },
+);
