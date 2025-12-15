6ee5067dbb8f7a4b84d2cc4f57e6fdebcaeb3745
Neo Nie <nihgwu@live.com>
2025-12-15 14:40:24 +0000

    move to fixtures

--- e2e/fixtures/styled-components/src/components/StyledComponentsRegistry.tsx
+++ e2e/fixtures/styled-components/src/components/StyledComponentsRegistry.tsx
@@ -1,36 +1,27 @@
 'use client';
 
 import type { ReactNode } from 'react';
-import type { AsyncLocalStorage } from 'node:async_hooks';
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
+import { insertServerHTML } from '../server-html/client';
 
-const serverStyleSheetStorage = (
-  globalThis as {
-    __SERVER_STYLE_SHEET_STORAGE__?: AsyncLocalStorage<{
-      sheet?: ServerStyleSheet;
-    }>;
-  }
-).__SERVER_STYLE_SHEET_STORAGE__;
-
-/**
- * Styled-components registry for Waku SSR.
- *
- * @see https://styled-components.com/docs/advanced#server-side-rendering
- */
-export function StyledComponentsRegistry({
+export const StyledComponentsRegistry = ({
   children,
 }: {
   children: ReactNode;
-}) {
-  let sheet: ServerStyleSheet | undefined;
+}) => {
   if (import.meta.env.SSR) {
-    const store = serverStyleSheetStorage?.getStore();
-    if (!store) {
-      throw new Error('serverStyleSheetStorage is missing.');
-    }
-    sheet = store.sheet ??= new ServerStyleSheet();
+    const sheet = new ServerStyleSheet();
+
+    insertServerHTML(() => {
+      const styles = sheet.getStyleTags();
+      sheet.instance.clearTag();
+      return styles;
+    });
+
+    return (
+      <StyleSheetManager sheet={sheet.instance}>{children}</StyleSheetManager>
+    );
   }
-  return (
-    <StyleSheetManager sheet={sheet?.instance}>{children}</StyleSheetManager>
-  );
-}
+
+  return <>{children}</>;
+};
--- examples/60_styled-components/src/pages/_layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { ReactNode } from 'react';
-import { StyledComponentsRegistry } from '../components/StyledComponentsRegistry';
-
-export default function RootLayout({ children }: { children: ReactNode }) {
-  return <StyledComponentsRegistry>{children}</StyledComponentsRegistry>;
-}
