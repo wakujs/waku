a9b4ca7e4dffc2e068dcc3ee1031ab51d37f0f3f
daishi <daishi@axlight.com>
2025-12-15 18:04:09 +0900

    change other adapters too

--- packages/waku/src/adapters/aws-lambda.ts
+++ packages/waku/src/adapters/aws-lambda.ts
@@ -3,24 +3,11 @@ import { serveStatic } from '@hono/node-server/serve-static';
 import { Hono } from 'hono';
 import type { MiddlewareHandler } from 'hono';
 import * as honoAwsLambda from 'hono/aws-lambda';
-import type { ImportGlobFunction } from 'vite/types/importGlob.d.ts';
 import {
   unstable_constants as constants,
   unstable_createServerEntryAdapter as createServerEntryAdapter,
   unstable_honoMiddleware as honoMiddleware,
 } from 'waku/internals';
-
-declare global {
-  interface ImportMeta {
-    glob: ImportGlobFunction;
-    readonly __WAKU_ORIGINAL_PATH__: string;
-  }
-}
-
-function joinPath(path1: string, path2: string) {
-  const p = path.posix.join(path1, path2);
-  return p.startsWith('/') ? p : './' + p;
-}
 
 const { DIST_PUBLIC } = constants;
 const { contextMiddleware, rscMiddleware, middlewareRunner } = honoMiddleware;
@@ -51,12 +38,8 @@ export default createServerEntryAdapter(
     }
     app.use(middlewareRunner(middlewareModules as never));
     app.use(rscMiddleware({ processRequest }));
-    const postBuildScript = joinPath(
-      import.meta.__WAKU_ORIGINAL_PATH__,
-      '../lib/aws-lambda-post-build.js',
-    );
     const postBuildArg: Parameters<
-      typeof import('./lib/aws-lambda-post-build.js').default
+      typeof import('./aws-lambda-post-build.js').default
     >[0] = {
       distDir: config.distDir,
     };
@@ -66,7 +49,7 @@ export default createServerEntryAdapter(
     return {
       fetch: app.fetch,
       build: processBuild,
-      postBuild: [postBuildScript, postBuildArg],
+      postBuild: ['waku/adapters/aws-lambda-post-build', postBuildArg],
     };
   },
 );
--- packages/waku/src/adapters/vercel.ts
+++ packages/waku/src/adapters/vercel.ts
@@ -1,25 +1,11 @@
-import path from 'node:path';
 import { getRequestListener } from '@hono/node-server';
 import { Hono } from 'hono';
 import type { MiddlewareHandler } from 'hono';
-import type { ImportGlobFunction } from 'vite/types/importGlob.d.ts';
 import {
   unstable_constants as constants,
   unstable_createServerEntryAdapter as createServerEntryAdapter,
   unstable_honoMiddleware as honoMiddleware,
 } from 'waku/internals';
-
-declare global {
-  interface ImportMeta {
-    glob: ImportGlobFunction;
-    readonly __WAKU_ORIGINAL_PATH__: string;
-  }
-}
-
-function joinPath(path1: string, path2: string) {
-  const p = path.posix.join(path1, path2);
-  return p.startsWith('/') ? p : './' + p;
-}
 
 const { DIST_PUBLIC } = constants;
 const { contextMiddleware, rscMiddleware, middlewareRunner } = honoMiddleware;
@@ -50,12 +36,8 @@ export default createServerEntryAdapter(
     }
     app.use(middlewareRunner(middlewareModules as never));
     app.use(rscMiddleware({ processRequest }));
-    const postBuildScript = joinPath(
-      import.meta.__WAKU_ORIGINAL_PATH__,
-      '../lib/vercel-post-build.js',
-    );
     const postBuildArg: Parameters<
-      typeof import('./lib/vercel-post-build.js').default
+      typeof import('./vercel-post-build.js').default
     >[0] = {
       assetsDir: options?.assetsDir || 'assets',
       distDir: config.distDir,
@@ -68,7 +50,7 @@ export default createServerEntryAdapter(
     return {
       fetch: app.fetch,
       build: processBuild,
-      postBuild: [postBuildScript, postBuildArg],
+      postBuild: ['waku/adapters/vercel-post-build', postBuildArg],
     };
   },
 );
