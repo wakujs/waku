9c6d5d45e4b1337c9bbbcf633b12d4f149b24774
Neo Nie <nihgwu@live.com>
2025-12-16 01:56:14 +0000

    test: simplify styled-components fixture (#1847)
    Follow up of https://github.com/wakujs/waku/discussions/1211
    putting everything together
    
    Alternative of https://github.com/wakujs/waku/pull/1831 without touching
    Waku. Key diffs:
    * use `async:hooks` instead of React Context
    * insert string instead of React elements
    
    This works for styled-components and emotion, for styled-jsx, it doesn't
    provide API to get style tag but only style element, so we need to use
    `renderToStaticMarkup` to convert to string first (didn't try)
    
    If we want to provide better support we should expose a way to
    interpolate `renderHTML` from SSR entry, which is used by both
    `handleRequest` and `handleBuild`
    
    Maybe it's already possible via middleware to interpolate the response
    steam? @dai-shi
    
    ----
    
    edit(daishi): #1845 has been merged and this PR is updated to apply the
    changes to the e2e test fixture.
    
    ---------
    
    Co-authored-by: Daishi Kato <dai-shi@users.noreply.github.com>

--- e2e/fixtures/styled-components/src/server-entry.ts
+++ e2e/fixtures/styled-components/src/server-entry.ts
@@ -1,52 +1,22 @@
-import { AsyncLocalStorage } from 'node:async_hooks';
-import { createRequire } from 'node:module';
-import { Readable } from 'node:stream';
-import type { ServerStyleSheet } from 'styled-components';
 import { fsRouter } from 'waku';
 import adapter from 'waku/adapters/default';
+import { injectRenderHtml } from './server-html/ssr';
 
-const serverStyleSheetStorage = new AsyncLocalStorage<{
-  sheet?: ServerStyleSheet;
-}>();
-(globalThis as any).__SERVER_STYLE_SHEET_STORAGE__ = serverStyleSheetStorage;
-globalThis.require = createRequire(import.meta.url);
-
-const patchRenderHtml =
-  <Args extends unknown[]>(
-    renderHtml: (...args: Args) => Promise<Response>,
-  ): typeof renderHtml =>
-  async (...args) => {
-    const res = await renderHtml(...args);
-    const sheet = serverStyleSheetStorage.getStore()?.sheet;
-    if (!sheet) {
-      throw new Error('ServerStyleSheet is not available');
-    }
-    const nodeReadable = Readable.fromWeb(res.body as never);
-    const newNodeReadable = sheet.interleaveWithNodeStream(nodeReadable);
-    const newBody = Readable.toWeb(newNodeReadable);
-    const newRes = new Response(newBody as never, {
-      status: res.status,
-      statusText: res.statusText,
-      headers: res.headers,
-    });
-    return newRes;
-  };
-
-const router = fsRouter(import.meta.glob('./**/*.tsx', { base: './pages' }));
+const router = fsRouter(
+  import.meta.glob('./**/*.{tsx,ts}', { base: './pages' }),
+);
 
 export default adapter({
-  handleRequest: async (input, utils) =>
-    serverStyleSheetStorage.run({}, () =>
-      router.handleRequest(input, {
-        ...utils,
-        renderHtml: patchRenderHtml(utils.renderHtml),
-      }),
-    ),
-  handleBuild: (utils) =>
-    router.handleBuild({
+  handleRequest(input, utils) {
+    return router.handleRequest(input, {
       ...utils,
-      renderHtml: patchRenderHtml(utils.renderHtml),
-      withRequest: <T>(req: Request, fn: () => T): T =>
-        serverStyleSheetStorage.run({}, () => utils.withRequest(req, fn)),
-    }),
+      renderHtml: injectRenderHtml(utils.renderHtml),
+    });
+  },
+  handleBuild(utils) {
+    return router.handleBuild({
+      ...utils,
+      renderHtml: injectRenderHtml(utils.renderHtml),
+    });
+  },
 });
