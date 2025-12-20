b192c19edb78b052fcc8cfd624c982d1d9e7aa51
jxom <7336481+jxom@users.noreply.github.com>
2025-12-20 17:20:37 +1100

    u

--- packages/waku/src/internals/index.ts
+++
@@ -1,3 +0,0 @@
-export { createServerEntryAdapter as unstable_createServerEntryAdapter } from '../lib/vite-rsc/handler.js';
-export * as unstable_constants from '../lib/constants.js';
-export * as unstable_honoMiddleware from '../lib/hono/middleware.js';
