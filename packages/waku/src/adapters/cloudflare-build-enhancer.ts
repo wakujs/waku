b985906dd850d53461dd067be0b7a73ae95cf884
Rob Marscher <rob@robmarscher.com>
2025-12-27 01:18:27 -0500

    No cf asset binding when static only

--- packages/waku/src/adapters/cloudflare-build-enhancer.ts
+++ packages/waku/src/adapters/cloudflare-build-enhancer.ts
@@ -54,15 +54,19 @@ ${
     ? `"main": ${JSON.stringify(forceRelativePath(path.relative(process.cwd(), mainEntry)))},
 `
     : ''
-}. // https://developers.cloudflare.com/workers/platform/compatibility-dates
+}  // https://developers.cloudflare.com/workers/platform/compatibility-dates
   "compatibility_date": "2025-11-17",
   // nodejs_als is required for Waku server-side request context
   // It can be removed if only building static pages
   "compatibility_flags": ["nodejs_als"],
   // https://developers.cloudflare.com/workers/static-assets/binding/
   "assets": {
-    "binding": "ASSETS",
-    "directory": "./${distDir}/${DIST_PUBLIC}",
+    ${
+      serverless
+        ? `"binding": "ASSETS",
+`
+        : ''
+    }    "directory": "./${distDir}/${DIST_PUBLIC}",
     "html_handling": "drop-trailing-slash"
   }
 }
