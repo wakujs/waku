b3e604916ca9517584f47d69314b3454a304a2a9
Hash <hash@hash.moe>
2026-01-22 22:59:47 +0800

    chore: use react-dom.preloadModule instead of import (#1918)

--- packages/waku/src/router/define-router.tsx
+++ packages/waku/src/router/define-router.tsx
@@ -203,13 +203,13 @@ const getRouterPrefetchCode = (path2moduleIds: Record<string, string[]>) => {
     path2idxs[path] = ids.map((id) => ids.indexOf(id));
   });
   return `
-globalThis.__WAKU_ROUTER_PREFETCH__ = (path) => {
+globalThis.__WAKU_ROUTER_PREFETCH__ = (path, callback) => {
   const ids = ${JSON.stringify(ids)};
   const path2idxs = ${JSON.stringify(path2idxs)};
   const pattern = Object.keys(path2idxs).find((key) => new RegExp(key).test(path));
   if (pattern && path2idxs[pattern]) {
     for (const idx of path2idxs[pattern] || []) {
-      import(ids[idx]);
+      callback(ids[idx]);
     }
   }
 };
