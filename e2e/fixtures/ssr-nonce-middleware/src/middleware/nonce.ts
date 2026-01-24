c516f71f3f08b6a935a7ae5954295a7859da3d7c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-01-24 15:52:19 +0900

    examples from https://github.com/wakujs/waku/pull/1923

---
+++ e2e/fixtures/ssr-nonce-middleware/src/middleware/nonce.ts
@@ -0,0 +1,24 @@
+import type { MiddlewareHandler } from 'hono';
+import { NONCE, secureHeaders } from 'hono/secure-headers';
+import { unstable_setNonce } from 'waku/router/server';
+
+const nonceMiddleware = (): MiddlewareHandler => {
+  const secure = secureHeaders({
+    contentSecurityPolicy: {
+      scriptSrc: ["'self'", NONCE],
+    },
+  });
+
+  return async (c, next) => {
+    await secure(c, async () => {
+      // Bridge Hono's nonce to Waku
+      const nonce = c.get('secureHeadersNonce');
+      if (nonce) {
+        unstable_setNonce(nonce);
+      }
+      await next();
+    });
+  };
+};
+
+export default nonceMiddleware;
