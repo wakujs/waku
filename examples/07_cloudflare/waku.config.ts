051f7752d1ef7d78fe157a909366481421d736ec
daishi <daishi@axlight.com>
2026-02-13 17:52:30 +0900

    fix lint

--- examples/07_cloudflare/waku.config.ts
+++ examples/07_cloudflare/waku.config.ts
@@ -1,10 +1,7 @@
-import fs from 'node:fs';
-import path from 'node:path';
 import { cloudflare } from '@cloudflare/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'waku/config';
-import type { VitePlugin } from 'waku/config';
 
 export default defineConfig({
   vite: {
