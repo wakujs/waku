49e75a972bc0afc8901cd8bd535e58fd2c02137e
daishi <daishi@axlight.com>
2026-04-05 11:26:28 +0100

    custom middleware example for append trailing slash

--- examples/02_template_js/src/middleware/no-trailing-slash.js
+++ examples/02_template_js/src/middleware/no-trailing-slash.js
@@ -1,3 +1,19 @@
 import { trimTrailingSlash } from 'hono/trailing-slash';
 
 export default () => trimTrailingSlash({ alwaysRedirect: true });
+
+// Usage of appendTrailingSlash
+/*
+import { appendTrailingSlash } from 'hono/trailing-slash';
+
+export default () => {
+  const middleware = appendTrailingSlash({ alwaysRedirect: true });
+  return (c, next) => {
+    const lastSegment = new URL(c.req.url).pathname.split('/').pop() || '';
+    if (lastSegment.includes('.')) {
+      return next();
+    }
+    return middleware(c, next);
+  };
+};
+*/
