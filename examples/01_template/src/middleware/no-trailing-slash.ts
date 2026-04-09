29cb9dae5bd6441c5d5c6880a4ef41adca56e360
Daishi Kato <dai-shi@users.noreply.github.com>
2026-04-09 22:14:00 +0900

    fix(adapters): revert the middleware order (#2007)
    revert the part of the changes in #1981
    close #2006

--- examples/01_template/src/middleware/no-trailing-slash.ts
+++ examples/01_template/src/middleware/no-trailing-slash.ts
@@ -1,3 +1,20 @@
 import { trimTrailingSlash } from 'hono/trailing-slash';
 
 export default () => trimTrailingSlash({ alwaysRedirect: true });
+
+// Usage of appendTrailingSlash
+/*
+import type { MiddlewareHandler } from 'hono';
+import { appendTrailingSlash } from 'hono/trailing-slash';
+
+export default (): MiddlewareHandler => {
+  const middleware = appendTrailingSlash({ alwaysRedirect: true });
+  return (c, next) => {
+    const lastSegment = new URL(c.req.url).pathname.split('/').pop() || '';
+    if (lastSegment.includes('.')) {
+      return next();
+    }
+    return middleware(c, next);
+  };
+};
+*/
--- packages/waku/src/adapters/bun.ts
+++ packages/waku/src/adapters/bun.ts
@@ -28,11 +28,6 @@ export default createServerEntryAdapter(
       }
       return c.text('404 Not Found', 404);
     });
-    app.use(contextMiddleware());
-    for (const middlewareFn of middlewareFns) {
-      app.use(middlewareFn());
-    }
-    app.use(middlewareRunner(middlewareModules as never));
     if (isBuild) {
       app.use(
         `${config.basePath}*`,
@@ -42,6 +37,11 @@ export default createServerEntryAdapter(
         }),
       );
     }
+    app.use(contextMiddleware());
+    for (const middlewareFn of middlewareFns) {
+      app.use(middlewareFn());
+    }
+    app.use(middlewareRunner(middlewareModules as never));
     app.use(rscMiddleware({ processRequest }));
     const buildOptions: BuildOptions = {
       distDir: config.distDir,
